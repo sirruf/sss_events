01628bb0fc8116e9e3d449b8b0cfffe2755f0b8f
Artem Kolesnikov <sirruf@me.com>
2016-09-12 11:46:42 +0300

    Localization Fix.

--- lib/events/engine.rb
+++ lib/events/engine.rb
@@ -1,6 +1,11 @@
 module Events
   class Engine < ::Rails::Engine
     isolate_namespace Events
+    config.to_prepare do
+      # Make the implementing application's helpers available to the engine.
+      # This is required for the overriding of engine views and helpers to work correctly.
+      Events::ApplicationController.helper Rails.application.helpers
+    end
     require 'rubygems'
     require 'russian'
     require 'acts_as_list'
